a34579c39be99b9d6d84845dd24f89a95e083b92
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-12 21:03:21 +0900

    Fetch home state and convert to UI state

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import HomeClient from "./HomeClient";
+import { fetchHomeState } from "./lib/api";
+import { toHomeUiState } from "./lib/homeStateUi";
 
 export default async function Page() {
   const homeState = await fetchHomeState();
