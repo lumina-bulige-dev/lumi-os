30b3b31452215d9d9f1090c6b6193a76b83c5b1f
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 16:36:51 +0900

    duplicate import of HomeClient
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import HomeClient from './HomeClient';
 import { fetchHomeState } from "./lib/api";
-import HomeClient from "./HomeClient";
+
 
 export default async function Page() {
   const homeState = await fetchHomeState();
