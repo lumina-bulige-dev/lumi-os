5fd56c46d949bdb9e92b288a5bae8e0f6ae9ef6a
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 19:50:14 +0900

    Remove unused openWise function from HomeClient
    Removed unused openWise function and related comments.
    
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
+import { fetchHomeState } from "./lib/api";
 
 export default function HomeClient() {
   const [state, setState] = useState<any>(null);
@@ -10,12 +10,7 @@ export default function HomeClient() {
     fetchHomeState().then(setState).catch(console.error);
   }, []);
 
-  const openWise = async () => {
-    const data = await fetchWiseLink();
-    const url = data.url ?? data.wise_url ?? data.wiseUrl;
-    if (url) window.open(url, "_blank", "noopener,noreferrer");
-  };
-
   if (!state) return <p>Loading...</p>;
-  // 以下 state を使って描画
+
+  // 以下 UI …
 }
