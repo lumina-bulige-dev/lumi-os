974ccdc76bf94035dc110f22cdceca52b070bbd3
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 19:25:20 +0900

    HomeClient.tsx
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { fetchHomeState, fetchWiseLink } from "./lib/api";
+
 
 export default function HomeClient() {
   const [state, setState] = useState<any>(null);
