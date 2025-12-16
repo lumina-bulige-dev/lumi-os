8008a6a1f835d64579c4ac985e1baca5c9be7d77
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 19:56:06 +0900

    Ensure fetchHomeState import is present
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { fetchHomeState } from "./lib/api";
+import { fetchHomeState } from "./lib/api"; // ← これが必須
 
 export default function HomeClient() {
   const [state, setState] = useState<any>(null);
