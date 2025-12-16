69dc72f0438fb2efd48608196ea8cbf0c5ba3707
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 20:15:13 +0900

    rendering HomeClient
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { fetchHomeState } from "./lib/api";
 import HomeClient from "./HomeClient";
 
-export default function Page() {
-  return <HomeClient />;
+export default async function Page() {
+  const homeState = await fetchHomeState();
+  return <HomeClient state={homeState} />;
 }
