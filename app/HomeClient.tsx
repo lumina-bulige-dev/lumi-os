a19e55eed2a7718b99d8b00a6e32ec86f0f11e41
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 20:42:46 +0900

    Implement HomeClient component with HomeStateUi
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -1,4 +1,7 @@
-// app/HomeClient.tsx
+"use client";
+
+import HomeStateUi from "./components/HomeStateUi";
+
 export default function HomeClient() {
-  ...
+  return <HomeStateUi balance={123456} />;
 }
