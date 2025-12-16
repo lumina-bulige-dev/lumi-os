99f58b51a5d067cab54a649b0abc69e28712c48c
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-16 20:41:01 +0900

    Update HomeStateUi to use balance prop
    Refactor HomeStateUi component to accept balance as a prop.
    
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- app/components/HomeStateUi.tsx
+++ app/components/HomeStateUi.tsx
@@ -1,5 +1,8 @@
 // app/components/HomeStateUi.tsx
-const data = await fetch(
-  "https://api.luminabulige.com/api/v1/core/home_state",
-  { cache: "no-store" }
-).then(r => r.json());
+type Props = {
+  balance: number;
+};
+
+export default function HomeStateUi({ balance }: Props) {
+  return <div>残高: ¥{balance.toLocaleString()}</div>;
+}
