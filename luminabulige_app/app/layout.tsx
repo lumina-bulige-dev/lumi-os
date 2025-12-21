8114cc372dab9e5eb7b05db9e9ab4c298f78b1a0
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-21 09:45:01 +0900

    Create RootLayout component with metadata
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

---
+++ luminabulige_app/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "LUMINA BULIGE",
+  description: "Money Flow Stabilizer OS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
