28064223831416b9a2ad44136f9b87aec334c91a
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-22 13:52:11 +0900

    Rename Page component to VPage and update fallback style
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- luminabulige_app/app/v/page.tsx
+++ luminabulige_app/app/v/page.tsx
@@ -2,9 +2,9 @@
 import { Suspense } from "react";
 import VClient from "./VClient";
 
-export default function Page() {
+export default function VPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div style={{ padding: 24 }}>Loading...</div>}>
       <VClient />
     </Suspense>
   );
