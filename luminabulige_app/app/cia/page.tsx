70d131ad7854ed2faef934f36e99da1d9888f3da
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-23 10:32:29 +0900

    Set runtime to edge for the CIA page
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- luminabulige_app/app/cia/page.tsx
+++ luminabulige_app/app/cia/page.tsx
@@ -1,5 +1,7 @@
-// luminabulige_app_/app/cia/page.tsx
+// luminabulige_app/app/cia/page.tsx
 import VClient from "./VClient";
+
+export const runtime = "edge";
 
 export default function Page() {
   return <VClient />;
