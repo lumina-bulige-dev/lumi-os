fdcd9cf65adcd5e197d664e7cdd1e839f4aebe94
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-23 10:56:55 +0900

    Remove redirect and add navigation links
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- luminabulige_app/app/page.tsx
+++ luminabulige_app/app/page.tsx
@@ -1,4 +1,15 @@
-import { redirect } from "next/navigation";
+// app/page.tsx
+import Link from "next/link";
+
 export default function Home() {
-  redirect("/cia");
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>LUMINA</h1>
+      <ul style={{ lineHeight: 2 }}>
+        <li><Link href="/cia">CIA Report</Link></li>
+        <li><Link href="/beta">Beta</Link></li>
+        <li><Link href="/compare">Compare</Link></li>
+      </ul>
+    </main>
+  );
 }
