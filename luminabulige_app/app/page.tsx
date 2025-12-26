f44810adc3c72ba11050a670572b69d840254f95
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-26 14:51:36 +0000

    Refactor Home component and export from lppage
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- luminabulige_app/app/page.tsx
+++ luminabulige_app/app/page.tsx
@@ -1,15 +1,7 @@
 // app/page.tsx
-import Link from "next/link";
+export const metadata = {
+  title: "LUMINA",
+  description: "LUMINA / LUMINA CIA / oKYC beta landing page.",
+};
 
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>LUMINA</h1>
-      <ul style={{ lineHeight: 2 }}>
-        <li><Link href="/cia">CIA Report</Link></li>
-        <li><Link href="/beta">Beta</Link></li>
-        <li><Link href="/compare">Compare</Link></li>
-      </ul>
-    </main>
-  );
-}
+export { default } from "./lppage";
