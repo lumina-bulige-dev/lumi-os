7473690f04610cf43adc4d77cf6add38dd583dc0
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-28 05:24:32 +0900

    Modify GET function to accept Request parameter
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- luminabulige_app/app/api/cia/route.ts
+++ luminabulige_app/app/api/cia/route.ts
@@ -1,8 +1,11 @@
 export const runtime = "edge";
 
-export async function GET() {
+export async function GET(req: Request) {
   return new Response("cia ok", {
     status: 200,
-    headers: { "content-type": "text/plain; charset=utf-8" },
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+      "x-cia-handler": "app/api/cia/route.ts",
+    },
   });
 }
