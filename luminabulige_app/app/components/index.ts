26fea66fbc422ecad1dadaf45f4aa27901bb9c6d
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-27 09:30:03 +0900

    Remove PromptChip export and adjust ParallelWorldDiagram
    Removed export of PromptChip and adjusted ParallelWorldDiagram export.
    
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

--- luminabulige_app/app/components/index.ts
+++ luminabulige_app/app/components/index.ts
@@ -1,6 +1,4 @@
-// app/components/index.ts
-export { default as TopNav } from "./TopNav";
 export { default as MoneyLogForm } from "./MoneyLogForm";
 export { default as BalanceBlock } from "./BalanceBlock";
-export { default as PromptChip } from "./PromptChip";
+// export { default as PromptChip } from "./PromptChip";
 export { default as ParallelWorldDiagram } from "./ParallelWorldDiagram";
