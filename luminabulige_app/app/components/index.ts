1916ba4d7322a271703ef0513d57827fa954327d
LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>
2025-12-27 09:03:31 +0900

    Create index.ts to export component modules
    Signed-off-by: LUMINABULIGE 「LLC仮」 <luminabulige@gmail.com>

---
+++ luminabulige_app/app/components/index.ts
@@ -0,0 +1,6 @@
+// app/components/index.ts
+export { default as TopNav } from "./TopNav";
+export { default as MoneyLogForm } from "./MoneyLogForm";
+export { default as BalanceBlock } from "./BalanceBlock";
+export { default as PromptChip } from "./PromptChip";
+export { default as ParallelWorldDiagram } from "./ParallelWorldDiagram";
