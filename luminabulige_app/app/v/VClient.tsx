508eba566bfcb68513954deb5725c492b2cd01ec
LUMINABULIGE <luminabulige@gmail.com>
2026-01-13 02:17:25 +0900

    Update VClient.tsx
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

--- luminabulige_app/app/v/VClient.tsx
+++ luminabulige_app/app/v/VClient.tsx
@@ -1,2 +1,6 @@
 import type { Result, VerifyResponse, ProofSummary } from "@/app/lib/verify-types";
 // もしくは相対パス: import type { ... } from "../lib/verify-types";
+
+export default function VClient() {
+  // ...
+}
