b5214b24016ae998be5f5aa5be830d9fa2ae37ca
LUMINABULIGE <luminabulige@gmail.com>
2026-01-13 05:29:26 +0900

    Create indexPublicAPI.ts
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

---
+++ app/features/verify/indexPublicAPI.ts
@@ -0,0 +1,4 @@
+export * from "./types/verify";
+export { default as VClient } from "./components/VClient";
+export { fetchVerify } from "./api/client";
+export { badgeStyle } from "./lib/badgeStyle";
