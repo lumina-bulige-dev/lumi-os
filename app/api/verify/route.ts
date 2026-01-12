5f8f49fff70b23dac35ee06ccaa4b8fc70bc70b2
LUMINABULIGE <luminabulige@gmail.com>
2026-01-13 04:55:05 +0900

    Set runtime to edge for verify route
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";       // これで静的化を禁止
 export const revalidate = 0;                  // キャッシュ無効
 export const fetchCache = "force-no-store";   // 取得キャッシュ回避（任意）
+export const runtime = "edge";
+
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
