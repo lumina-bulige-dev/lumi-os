ddf5b5ba99796ac07663dce5564aab623cf656bf
LUMINABULIGE <luminabulige@gmail.com>
2026-01-13 16:38:37 +0900

    Use search params to display token in VClient
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

--- luminabulige_app/app/v/VClient.tsx
+++ luminabulige_app/app/v/VClient.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import type { Result, VerifyResponse, ProofSummary } from "@/app/lib/verify-types";
-// もしくは相対パス: import type { ... } from "../lib/verify-types";
+import { useSearchParams } from "next/navigation";
 
 export default function VClient() {
-  // ...
+  const sp = useSearchParams();
+  const token = sp.get("token");
+  return <div>{token ? `token=${token}` : "no token"}</div>;
 }
