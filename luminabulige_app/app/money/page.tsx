bb6a7dc14f1647a70395836d387ff06c7c973657
LUMINABULIGE <luminabulige@gmail.com>
2026-01-13 23:54:51 +0900

    Update page.tsx
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

--- luminabulige_app/app/money/page.tsx
+++ luminabulige_app/app/money/page.tsx
@@ -1,1 +1,5 @@
 
+export { default as MoneyLogForm } from "./MoneyLogForm";
+export { default as BalanceBlock } from "./BalanceBlock";
+// export { default as PromptChip } from "./PromptChip";
+export { default as ParallelWorldDiagram } from "./ParallelWorldDiagram";
