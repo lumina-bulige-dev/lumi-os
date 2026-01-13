9d46d5a82565e85b05d0c5b6a97c740d8b1888ec
LUMINABULIGE <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-01-14 03:17:54 +0900

    Update VClient.tsx
    Signed-off-by: LUMINABULIGE  <248264876+lumina-bulige-dev@users.noreply.github.com>

--- luminabulige_app/app/cia/VClient.tsx
+++ luminabulige_app/app/cia/VClient.tsx
@@ -57,24 +57,24 @@ import { ui } from "../ui";
 
 export const resultPalette = {
   OK: {
-    color: ui.color.ok,
-    bg: ui.color.okBg,
-    br: "#A7F3D0",
+  //  color: ui.color.ok,
+  //  bg: ui.color.okBg,
+   // br: "#A7F3D0",
   },
   NG: {
-    color: "#F97316",
-    bg: "rgba(251,146,60,0.16)",
-    br: "#FED7AA",
+ //   color: "#F97316",
+  //  bg: "rgba(251,146,60,0.16)",
+  //  br: "#FED7AA",
   },
   REVOKED: {
-    color: "#DC2626",
-    bg: "rgba(220,38,38,0.16)",
-    br: "#FECACA",
+ //   color: "#DC2626",
+  ///  bg: "rgba(220,38,38,0.16)",
+  //  br: "#FECACA",
   },
   UNKNOWN: {
-    color: "#64748B",
-    bg: "rgba(100,116,139,0.16)",
-    br: "#CBD5E1",
+  //  color: "#64748B",
+  //  bg: "rgba(100,116,139,0.16)",
+  //  br: "#CBD5E1",
   },
 } as const;
 
