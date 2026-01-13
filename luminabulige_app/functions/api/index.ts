963d3b437e88468eddf7fb17bfd515eed5f424bf
LUMINABULIGE <luminabulige@gmail.com>
2026-01-13 20:49:49 +0900

    Implement CORS support in API
    Add CORS headers to API responses
    
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

--- luminabulige_app/functions/api/index.ts
+++ luminabulige_app/functions/api/index.ts
@@ -1,4 +1,14 @@
 cat > functions/api/index.ts <<'EOF'
+const cors = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET,POST,OPTIONS",
+  "access-control-allow-headers": "Content-Type, Authorization",
+};
+
+if (req.method === "OPTIONS") return new Response(null, { headers: cors });
+
+return new Response(JSON.stringify(...), { headers: { ...cors, "content-type": "application/json; charset=utf-8" }});
+
 export default {
   async fetch(req: Request) {
     const url = new URL(req.url);
