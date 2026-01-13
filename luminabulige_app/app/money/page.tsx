7ba11b4a3838f09aca10f57d57abfd1dc53441b2
LUMINABULIGE <luminabulige@gmail.com>
2026-01-14 00:08:30 +0900

    Update page.tsx
    Signed-off-by: LUMINABULIGE  <luminabulige@gmail.com>

--- luminabulige_app/app/money/page.tsx
+++ luminabulige_app/app/money/page.tsx
@@ -1,6 +1,12 @@
-const Page = () => { ... }
-export default Page
-export { default as MoneyLogForm } from "./MoneyLogForm";
-export { default as BalanceBlock } from "./BalanceBlock";
+export default function MoneyPage() {
+  return (
+    <div>
+      {/* MoneyLogForm や BalanceBlock */}
+    </div>
+  )
+}
+
+//export { default as MoneyLogForm } from "./MoneyLogForm";
+//export { default as BalanceBlock } from "./BalanceBlock";
 // export { default as PromptChip } from "./PromptChip";
-export { default as ParallelWorldDiagram } from "./ParallelWorldDiagram";
+//export { default as ParallelWorldDiagram } from "./ParallelWorldDiagram";
