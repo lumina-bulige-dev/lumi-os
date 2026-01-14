48b5f3a3d43dce87f07d012658d51a5bdf97e0e1
LUMINABULIGE <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-01-14 09:39:03 +0900

    Update page.tsx
    Signed-off-by: LUMINABULIGE  <248264876+lumina-bulige-dev@users.noreply.github.com>

--- luminabulige_app/app/v/page.tsx
+++ luminabulige_app/app/v/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 import { Suspense } from "react";
 import VClient from "./VClient";
-
-export default function Page() {
-  return (
-    <Suspense fallback={<div />}>
-      <VClient />
-    </Suspense>
-  );
-}
-
+<Suspense fallback={...}>
+  <VClient />
+</Suspense>
