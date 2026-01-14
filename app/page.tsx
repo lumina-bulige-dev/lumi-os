8597bcdb0a222ff4d6b7302d42c0ddf16b43ac2e
LUMINABULIGE <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-01-14 13:30:58 +0900

    Rename import from ui to ciaUi
    Signed-off-by: LUMINABULIGE  <248264876+lumina-bulige-dev@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // app/page.tsx
 import HomeClient from "./HomeClient";
-import { ui } from "@/app/cia/ui";
+import { ciaUi } from "@/app/cia/ciaUi";
 export default function Page() {
   return <HomeClient />;
 }
