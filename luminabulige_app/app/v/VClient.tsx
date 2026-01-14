682102bdbdde108b01f0e7aff73d276385092080
LUMINABULIGE <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-01-14 09:54:56 +0900

    Implement proofId state in VClient component
    Add proofId state using useMemo for search params.
    
    Signed-off-by: LUMINABULIGE  <248264876+lumina-bulige-dev@users.noreply.github.com>

--- luminabulige_app/app/v/VClient.tsx
+++ luminabulige_app/app/v/VClient.tsx
@@ -1,4 +1,11 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useMemo, useState } from "react";
 
+export default function VClient() {
+  const sp = useSearchParams();
+  const proofId = useMemo(() => sp.get("proofId") || "", [sp]);
+
+  // 以下そのまま
+}
