8f4664cbc99358eebb0993d9957366b6b3bb9aac
LUMINABULIGE <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-01-14 09:38:08 +0900

    Replace SearchParamsConsumer with VClient in Page
    Signed-off-by: LUMINABULIGE  <248264876+lumina-bulige-dev@users.noreply.github.com>

--- luminabulige_app/app/v/page.tsx
+++ luminabulige_app/app/v/page.tsx
@@ -1,21 +1,12 @@
-// app/v/page.tsx
-import { Suspense } from 'react';
-import SearchParamsConsumer from './SearchParamsConsumer';
+"use client";
+import { Suspense } from "react";
+import VClient from "./VClient";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading…</div>}>
-      <SearchParamsConsumer />
+    <Suspense fallback={<div />}>
+      <VClient />
     </Suspense>
   );
-}
-
-// app/v/SearchParamsConsumer.tsx
-'use client';
-import { useSearchParams } from 'next/navigation';
-
-export default function SearchParamsConsumer() {
-  const params = useSearchParams();
-  // … ここで params を使う処理 …
 }
 
