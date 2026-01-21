8dabf221968c20e7b1abfc337b6743372a42d6a6
TOSHIAKI  NISHINO <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-01-21 14:39:53 +0900

    Update next.config.mjs
    Signed-off-by: TOSHIAKI  NISHINO <248264876+lumina-bulige-dev@users.noreply.github.com>

--- luminabulige_app/next.config.mjs
+++ luminabulige_app/next.config.mjs
@@ -1,4 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
+  reactStrictMode: true
 };
-module.exports = nextConfig;
+
+export default nextConfig;
