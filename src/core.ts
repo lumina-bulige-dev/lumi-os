7e958620737d498e71c0753b711ec8e9f0917282
TOSHIAKI  NISHINO <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-02-27 18:09:42 +0900

    Export modules from lumi-os-core
    Signed-off-by: TOSHIAKI  NISHINO <248264876+lumina-bulige-dev@users.noreply.github.com>

---
+++ src/core.ts
@@ -0,0 +1,3 @@
+export * as AION from '../lumi-os-core/aion';
+export * as LIGHT_ARCH from '../lumi-os-core/architecture/light';
+export * as QUQTEXFOLCON from '../lumi-os-core/crypto/quqtexfolcon/stack';
