699317fd49af5d68bb8c3aa5cbd125d6e6175024
TOSHIAKI  NISHINO <248264876+lumina-bulige-dev@users.noreply.github.com>
2026-02-27 18:12:57 +0900

    Create __lumi-core.ts to export core modules
    Signed-off-by: TOSHIAKI  NISHINO <248264876+lumina-bulige-dev@users.noreply.github.com>

---
+++ src/internal/__lumi-core.ts
@@ -0,0 +1,9 @@
+import * as AION from "../../../lumi-os-core/aion";
+import * as LIGHT_ARCH from "../../../lumi-os-core/architecture/light";
+import * as QUQTEXFOLCON from "../../../lumi-os-core/crypto/quqtexfolcon/stack";
+
+export const __LumiCore = {
+  AION,
+  LIGHT_ARCH,
+  QUQTEXFOLCON,
+};
